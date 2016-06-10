fb2affc6b265c5df50ef5f0099e97f57dfa461c3
rayGreycloud <rayGreycloud@gmail.com>
2016-06-10 15:20:12 -0700

    initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+    return (
+      <div className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className='menu'>
+            <li className='menu-text'>
+              React Timer App
+            </li>
+            <li>
+              <IndexLink to='/' activeClassName='active-link'>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to='/' activeClassName='active-link'>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <ul classname='menu'>
+            <li classname='menu-text'>
+              Created by <a href='https://github.com/rayGreycloud/timer-react' target='_blank'>Ray Greycloud</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+module.exports = Navigation;
